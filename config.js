96aff4e11f0036656250108be6a5b96301160c9a
kololia237 <94543917+kololia237@users.noreply.github.com>
2024-04-22 14:31:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const Config = {
-    newsUrl: '',
-    galleryUrl: '',
+    newsUrl: 'https://raw.githubusercontent.com/kololia237/MobileApp/main/data/news.json?token=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',
+    galleryUrl: 'https://raw.githubusercontent.com/kololia237/MobileApp/main/data/images.json?token=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',
 };
 
 export default Config;
